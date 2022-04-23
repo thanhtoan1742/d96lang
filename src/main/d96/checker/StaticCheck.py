d2c54d3fe2cc2d14421897a7ea3ab5d7b4bb3d4e
thanhtoan1742 <thanhtoan1742@gmail.com>
2022-04-23 23:48:11 +0700

    init 2 pass checker implement basic symbol pool

--- src/main/d96/checker/StaticCheck.py
+++ src/main/d96/checker/StaticCheck.py
@@ -1,61 +1,218 @@
 
-"""
- * @author nhphung
-"""
-from AST import * 
+import AST
 from Visitor import *
 from Utils import Utils
 from StaticError import *
+from dataclasses import dataclass
+
+"""
+what happens when a method returns multiple time and the return types are diffrent?
+what happens when a method is a recursive loop with no end point?
+"""
+
+
+@dataclass
+class MethodDeclRet(AST.MethodDecl):
+    retType: AST.Type = AST.VoidType()
+
+
 
 class MType:
-    def __init__(self,partype,rettype):
+    def __init__(self, partype, rettype):
         self.partype = partype
         self.rettype = rettype
 
+
 class Symbol:
-    def __init__(self,name,mtype,value = None):
+    def __init__(self, name, mtype, value=None):
         self.name = name
         self.mtype = mtype
         self.value = value
 
-class StaticChecker(BaseVisitor,Utils):
 
-    global_envi = [
-    Symbol("getInt",MType([],IntType())),
-    Symbol("putIntLn",MType([IntType()],VoidType()))
-    ]
-            
-    
-    def __init__(self,ast):
+class SymbolPool:
+    def __init__(self) -> None:
+        self.scope = 0
+        self.pools[self.scope] = {}
+
+    def inc_scope(self):
+        self.scope += 1
+        self.pools[self.scope] = {}
+
+    def dec_scope(self):
+        self.pools.pop(self.scope)
+        self.scope -= 1
+
+    def is_declared_in_current_scope(self, id):
+        return id in self.pools[self.scope]
+
+    def get_symbol(self, id):
+        for s in range(self.scope, -1, -1):
+            if id in self.pools[s]:
+                return self.pools[s][id]
+        return None
+
+
+
+
+class StaticChecker(BaseVisitor, Utils):
+    def __init__(self, ast):
         self.ast = ast
 
- 
-    
     def check(self):
-        return self.visit(self.ast,StaticChecker.global_envi)
+        ast = FirstPassChecker().visit(self.ast, {})
+        SecondPassChecker().visit(ast, {})
 
-    def visitProgram(self,ast, c): 
-        return [self.visit(x,c) for x in ast.decl]
 
-    def visitFuncDecl(self,ast, c): 
-        return list(map(lambda x: self.visit(x,(c,True)),ast.body.stmt)) 
-    
+# visit the given AST then return a new AST where all type a resolved.
+# do some memantic check also
+class FirstPassChecker(BaseVisitor, Utils):
+    def visitAST(self, ctx, visit_params):
+        pass
 
-    def visitCallExpr(self, ast, c): 
-        at = [self.visit(x,(c[0],False)) for x in ast.param]
-        
-        res = self.lookup(ast.method.name,c[0],lambda x: x.name)
-        if res is None or not type(res.mtype) is MType:
-            raise Undeclared(Function(),ast.method.name)
-        elif len(res.mtype.partype) != len(at):
-            if c[1]:
-                raise TypeMismatchInStatement(ast)
-            else:
-                raise TypeMismatchInExpression(ast)
-        else:
-            return res.mtype.rettype
+    def visitInst(self, ctx, visit_params):
+        pass
 
-    def visitIntLiteral(self,ast, c): 
-        return IntType()
-    
+    def visitStmt(self, ctx, visit_params):
+        pass
 
+    def visitExpr(self, ctx, visit_params):
+        pass
+
+    def visitLHS(self, ctx, visit_params):
+        pass
+
+    def visitType(self, ctx, visit_params):
+        pass
+
+    def visitMemDecl(self, ctx, visit_params):
+        pass
+
+    def visitId(self, ctx, visit_params):
+        pass
+
+    def visitBinaryOp(self, ctx, visit_params):
+        pass
+
+    def visitUnaryOp(self, ctx, visit_params):
+        pass
+
+    def visitCallExpr(self, ctx, visit_params):
+        pass
+
+    def visitNewExpr(self, ctx, visit_params):
+        pass
+
+    def visitArrayCell(self, ctx, visit_params):
+        pass
+
+    def visitFieldAccess(self, ctx, visit_params):
+        pass
+
+    def visitLiteral(self, ctx, visit_params):
+        pass
+
+    def visitIntLiteral(self, ctx, visit_params):
+        pass
+
+    def visitFloatLiteral(self, ctx, visit_params):
+        pass
+
+    def visitStringLiteral(self, ctx, visit_params):
+        pass
+
+    def visitBooleanLiteral(self, ctx, visit_params):
+        pass
+
+    def visitNullLiteral(self, ctx, visit_params):
+        pass
+
+    def visitSelfLiteral(self, ctx, visit_params):
+        pass
+
+    def visitArrayLiteral(self, ctx, visit_params):
+        pass
+
+    def visitDecl(self, ctx, visit_params):
+        pass
+
+    def visitStoreDecl(self, ctx, visit_params):
+        pass
+
+    def visitAssign(self, ctx, visit_params):
+        pass
+
+    def visitIf(self, ctx, visit_params):
+        pass
+
+    def visitFor(self, ctx, visit_params):
+        pass
+
+    def visitBreak(self, ctx, visit_params):
+        pass
+
+    def visitContinue(self, ctx, visit_params):
+        pass
+
+    def visitReturn(self, ctx, visit_params):
+        pass
+
+    def visitCallStmt(self, ctx, visit_params):
+        pass
+
+    def visitVarDecl(self, ctx, visit_params):
+        pass
+
+    def visitBlock(self, ctx, visit_params):
+        pass
+
+    def visitConstDecl(self, ctx, visit_params):
+        pass
+
+    def visitClassDecl(self, ctx, visit_params):
+        pass
+
+    def visitSIKind(self, ctx, visit_params):
+        pass
+
+    def visitInstance(self, ctx, visit_params):
+        pass
+
+    def visitStatic(self, ctx, visit_params):
+        pass
+
+    def visitMethodDecl(self, ctx, visit_params):
+        pass
+
+    def visitAttributeDecl(self, ctx, visit_params):
+        pass
+
+    def visitIntType(self, ctx, visit_params):
+        pass
+
+    def visitFloatType(self, ctx, visit_params):
+        pass
+
+    def visitBoolType(self, ctx, visit_params):
+        pass
+
+    def visitStringType(self, ctx, visit_params):
+        pass
+
+    def visitArrayType(self, ctx, visit_params):
+        pass
+
+    def visitClassType(self, ctx, visit_params):
+        pass
+
+    def visitVoidType(self, ctx, visit_params):
+        pass
+
+    def visitProgram(self, ctx, visit_params):
+        for d in ctx.decl:
+            self.visit(d, visit_params)
+
+
+
+class SecondPassChecker(BaseVisitor, Utils):
+    pass
