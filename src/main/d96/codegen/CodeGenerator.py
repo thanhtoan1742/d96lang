1f687ca5b33abcaa456513d4831c83f3fa411921
thanhtoan1742 <thanhtoan1742@gmail.com>
2022-05-23 20:36:19 +0700

    init codegen

--- src/main/d96/codegen/CodeGenerator.py
+++ src/main/d96/codegen/CodeGenerator.py
@@ -12,16 +12,19 @@ from Emitter import Emitter
 from Frame import Frame
 from abc import ABC, abstractmethod
 
+
 class CodeGenerator(Utils):
     def __init__(self):
         self.libName = "io"
 
     def init(self):
         return [Symbol("getInt", MType(list(), IntType()), CName(self.libName)),
-                    Symbol("putInt", MType([IntType()], VoidType()), CName(self.libName)),
-                    Symbol("putIntLn", MType([IntType()], VoidType()), CName(self.libName))
-                    
-                    ]
+                Symbol("putInt", MType([IntType()],
+                       VoidType()), CName(self.libName)),
+                Symbol("putIntLn", MType([IntType()],
+                       VoidType()), CName(self.libName))
+
+                ]
 
     def gen(self, ast, dir_):
         #ast: AST
@@ -31,13 +34,15 @@ class CodeGenerator(Utils):
         gc = CodeGenVisitor(ast, gl, dir_)
         gc.visit(ast, None)
 
+
 class StringType(Type):
-    
+
     def __str__(self):
         return "StringType"
 
     def accept(self, v, param):
         return None
+
 
 class ArrayPointerType(Type):
     def __init__(self, ctype):
@@ -49,14 +54,19 @@ class ArrayPointerType(Type):
 
     def accept(self, v, param):
         return None
+
+
 class ClassType(Type):
-    def __init__(self,cname):
+    def __init__(self, cname):
         self.cname = cname
+
     def __str__(self):
         return "Class({0})".format(str(self.cname))
+
     def accept(self, v, param):
         return None
-        
+
+
 class SubBody():
     def __init__(self, frame, sym):
         #frame: Frame
@@ -64,6 +74,7 @@ class SubBody():
 
         self.frame = frame
         self.sym = sym
+
 
 class Access():
     def __init__(self, frame, sym, isLeft, isFirst):
@@ -77,8 +88,10 @@ class Access():
         self.isLeft = isLeft
         self.isFirst = isFirst
 
+
 class Val(ABC):
     pass
+
 
 class Index(Val):
     def __init__(self, value):
@@ -86,11 +99,13 @@ class Index(Val):
 
         self.value = value
 
+
 class CName(Val):
     def __init__(self, value):
         #value: String
 
         self.value = value
+
 
 class CodeGenVisitor(BaseVisitor, Utils):
     def __init__(self, astTree, env, dir_):
@@ -108,12 +123,14 @@ class CodeGenVisitor(BaseVisitor, Utils):
         #ast: Program
         #c: Any
 
-        self.emit.printout(self.emit.emitPROLOG(self.className, "java.lang.Object"))
+        self.emit.printout(self.emit.emitPROLOG(
+            self.className, "java.lang.Object"))
         e = SubBody(None, self.env)
         for x in ast.decl:
             e = self.visit(x, e)
         # generate default constructor
-        self.genMETHOD(FuncDecl(Id("<init>"), list(), None, Block(list(), list())), c, Frame("<init>", VoidType))
+        self.genMETHOD(FuncDecl(Id("<init>"), list(), None, Block(
+            list(), list())), c, Frame("<init>", VoidType))
         self.emit.emitEPILOG()
         return c
 
@@ -123,13 +140,15 @@ class CodeGenVisitor(BaseVisitor, Utils):
         #frame: Frame
 
         isInit = consdecl.returnType is None
-        isMain = consdecl.name.name == "main" and len(consdecl.param) == 0 and type(consdecl.returnType) is VoidType
+        isMain = consdecl.name.name == "main" and len(
+            consdecl.param) == 0 and type(consdecl.returnType) is VoidType
         returnType = VoidType() if isInit else consdecl.returnType
         methodName = "<init>" if isInit else consdecl.name.name
         intype = [ArrayPointerType(StringType())] if isMain else list()
         mtype = MType(intype, returnType)
 
-        self.emit.printout(self.emit.emitMETHOD(methodName, mtype, not isInit, frame))
+        self.emit.printout(self.emit.emitMETHOD(
+            methodName, mtype, not isInit, frame))
 
         frame.enterScope(True)
 
@@ -137,16 +156,19 @@ class CodeGenVisitor(BaseVisitor, Utils):
 
         # Generate code for parameter declarations
         if isInit:
-            self.emit.printout(self.emit.emitVAR(frame.getNewIndex(), "this", ClassType(self.className), frame.getStartLabel(), frame.getEndLabel(), frame))
+            self.emit.printout(self.emit.emitVAR(frame.getNewIndex(), "this", ClassType(
+                self.className), frame.getStartLabel(), frame.getEndLabel(), frame))
         if isMain:
-            self.emit.printout(self.emit.emitVAR(frame.getNewIndex(), "args", ArrayPointerType(StringType()), frame.getStartLabel(), frame.getEndLabel(), frame))
+            self.emit.printout(self.emit.emitVAR(frame.getNewIndex(), "args", ArrayPointerType(
+                StringType()), frame.getStartLabel(), frame.getEndLabel(), frame))
 
         body = consdecl.body
         self.emit.printout(self.emit.emitLABEL(frame.getStartLabel(), frame))
 
         # Generate code for statements
         if isInit:
-            self.emit.printout(self.emit.emitREADVAR("this", ClassType(self.className), 0, frame))
+            self.emit.printout(self.emit.emitREADVAR(
+                "this", ClassType(self.className), 0, frame))
             self.emit.printout(self.emit.emitINVOKESPECIAL(frame))
         list(map(lambda x: self.visit(x, SubBody(frame, glenv)), body.stmt))
 
@@ -154,7 +176,7 @@ class CodeGenVisitor(BaseVisitor, Utils):
         if type(returnType) is VoidType:
             self.emit.printout(self.emit.emitRETURN(VoidType(), frame))
         self.emit.printout(self.emit.emitENDMETHOD(frame))
-        frame.exitScope();
+        frame.exitScope()
 
     def visitFuncDecl(self, ast, o):
         #ast: FuncDecl
@@ -174,7 +196,7 @@ class CodeGenVisitor(BaseVisitor, Utils):
         nenv = ctxt.sym
         sym = self.lookup(ast.method.name, nenv, lambda x: x.name)
         cname = sym.value.value
-    
+
         ctype = sym.mtype
 
         in_ = ("", list())
@@ -182,7 +204,8 @@ class CodeGenVisitor(BaseVisitor, Utils):
             str1, typ1 = self.visit(x, Access(frame, nenv, False, True))
             in_ = (in_[0] + str1, in_[1].append(typ1))
         self.emit.printout(in_[0])
-        self.emit.printout(self.emit.emitINVOKESTATIC(cname + "/" + ast.method.name, ctype, frame))
+        self.emit.printout(self.emit.emitINVOKESTATIC(
+            cname + "/" + ast.method.name, ctype, frame))
 
     def visitIntLiteral(self, ast, o):
         #ast: IntLiteral
@@ -191,5 +214,3 @@ class CodeGenVisitor(BaseVisitor, Utils):
         ctxt = o
         frame = ctxt.frame
         return self.emit.emitPUSHICONST(ast.value, frame), IntType()
-
-    
